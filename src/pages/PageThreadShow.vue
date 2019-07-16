9abb54b30a87b2c651326775e09108a08ee1d41c
Felipe Balbi <balbif@gmail.com>
2019-07-16 14:21:12 +0300

    let the discussion begin
    Signed-off-by: Felipe Balbi <balbif@gmail.com>

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,7 +1,25 @@
 <template>
 <div class="col-large push-top">
   <h1>{{ thread.title }}</h1>
+
   <PostList :posts="posts"/>
+
+  <form @submit.prevent="addPost">
+    <div class="form-group">
+      <textarea
+	name=""
+	id=""
+	cols="30"
+	row="10"
+	class="form-input"
+	v-model="newPostText">
+      </textarea>
+    </div>
+
+    <div class="form-actions">
+      <button class="btn-blue">Submit</button>
+    </div>
+  </form>
 </div>
 </template>
 
@@ -9,6 +27,8 @@
 <script>
 import sourceData from '@/data.json'
 import PostList from '@/components/PostList'
+
+console.log(sourceData)
 
 export default {
   components: {
@@ -22,14 +42,33 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      newPostText: ''
     }
   },
   computed: {
     posts () {
       const postIds = Object.values(this.thread.posts)
-
       return Object.values(sourceData.posts).filter(post => postIds.includes(post['.key']))
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = 'greatPost' + Math.random()
+
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3',
+        '.key': postId
+      }
+
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      this.$set(sourceData.users[post.userId].posts, postId, postId)
+
+      this.newPostText = ''
     }
   }
 }
