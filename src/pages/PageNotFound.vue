05adf18b0a3efaba0efaef82610b9a945f110d88
Felipe Balbi <balbif@gmail.com>
2019-07-16 12:32:39 +0300

    css modules with webpack
    Signed-off-by: Felipe Balbi <balbif@gmail.com>

--- src/pages/PageNotFound.vue
+++ src/pages/PageNotFound.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="col-full">
-  <h1>Not Found</h1>
+<div class="col-full" :class="$style.centered">
+  <h1 :class="$style.headingLarge">Not Found</h1>
   
-  <p>
+  <p :class="$style.textLarge">
     Oops, we couldn't find what you are looking for. Why don't you
     <router-link :to="{name: 'Home'}">
       go Home instead
@@ -15,17 +15,17 @@
   export default {}
 </script>
 
-<style scoped>
-  h1 {
+<style module>
+  .headingLarge {
       font-size: 100px;
   }
 
-  p {
+  .textLarge {
       font-size: 50px;
       font-weight: 100;
   }
 
-  div {
+  .centered {
       text-align: center;
   }
 </style>
