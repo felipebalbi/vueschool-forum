5466804e427977dcf83afba55e4bb4460771bbb9
Felipe Balbi <balbif@gmail.com>
2019-07-17 17:23:59 +0300

    categories, collections of forums
    Signed-off-by: Felipe Balbi <balbif@gmail.com>

---
+++ src/pages/PageCategory.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="category-wrapper">
+  <div class="col-full push-top">
+    <h1>{{category.name}}</h1>
+  </div>
+
+  <div class="col-full">
+    <CategoryListItem :category="category" />
+  </div>
+</div>
+</template>
+
+<script>
+import sourceData from '@/data'
+import CategoryListItem from '@/components/CategoryListItem'
+
+export default {
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+
+  computed: {
+    category () {
+      return sourceData.categories[this.id]
+    }
+  },
+
+  components: {
+    CategoryListItem
+  }
+}
+</script>
+
+<style scoped>
+  .category-wrapper {
+      width: 100%;
+  }
+</style>
