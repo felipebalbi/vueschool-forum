cdc660d05bf780432ca6626a24f31babca239655
Felipe Balbi <balbif@gmail.com>
2019-07-20 15:52:14 +0300

    The NavBar
    Signed-off-by: Felipe Balbi <balbif@gmail.com>

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,15 +1,16 @@
 <template>
-<div class="col-large push-top">
-  <h1>{{ thread.title }}</h1>
-
-  <p>
-    By <a href="#" class="link-unstyled">Robin</a>, <AppDate :timestamp="thread.publishedAt"/>
-    <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">3 replies by 3 contributors</span>
-  </p>
+<div>
+  <div class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <p>
+      By <a href="#" class="link-unstyled">Robin</a>, <AppDate :timestamp="thread.publishedAt"/>
+      <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">3 replies by 3 contributors</span>
+    </p>
   
-  <PostList :posts="posts"/>
-  <PostEditor
-    :threadId="id"/>
+    <PostList :posts="posts"/>
+    <PostEditor
+      :threadId="id"/>
+  </div>
 </div>
 </template>
 
