79c36eff2f9176abe6ad64aeabc749d98570508c
Felipe Balbi <balbif@gmail.com>
2019-07-23 18:40:11 +0300

    Use authId when Creating Posts

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -36,18 +36,16 @@ export default {
     }
   },
 
-  data () {
-    return {
-      thread: this.$store.state.threads[this.id]
-    }
-  },
-
   computed: {
     posts () {
       const postIds = Object.values(this.thread.posts)
       return Object.values(this.$store.state.posts).filter(post =>
         postIds.includes(post['.key'])
       )
+    },
+
+    thread () {
+      return this.$store.state.threads[this.id]
     }
   }
 }
