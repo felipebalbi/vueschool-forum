1e89a20fd2b52138cad9c31f896f434851f4950f
Felipe Balbi <balbi@kernel.org>
2019-07-24 21:32:22 +0300

    Use Route Params

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -34,9 +34,9 @@
 <script>
 export default {
   props: {
-    forum: {
+    forumId: {
       required: true,
-      type: Object
+      type: String
     }
   },
 
@@ -44,6 +44,12 @@ export default {
     return {
       title: '',
       text: ''
+    }
+  },
+
+  computed: {
+    forum () {
+      return this.$store.state.forums[this.forumId]
     }
   },
 
