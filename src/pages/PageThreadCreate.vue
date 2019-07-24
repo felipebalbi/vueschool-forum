0ee2eae51eedc5571d2cea1f3843e67c3cd07f6c
Felipe Balbi <balbif@gmail.com>
2019-07-24 14:01:28 +0300

    Redirect on Publish

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -24,7 +24,7 @@
       </div>
 
       <div class="btn-group">
-        <button class="btn btn-ghost">Cancel</button>
+        <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
         <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
       </div>
     </form>
@@ -49,11 +49,22 @@ export default {
 
   methods: {
     save () {
-      this.$store.dispatch('createThread', {
-        forumId: this.forum['.key'],
-        title: this.title,
-        text: this.text
-      })
+      this.$store
+        .dispatch('createThread', {
+          forumId: this.forum['.key'],
+          title: this.title,
+          text: this.text
+        })
+        .then(thread => {
+          this.$router.push({
+            name: 'ThreadShow',
+            params: { id: thread['.key'] }
+          })
+        })
+    },
+
+    cancel () {
+      this.$router.push({ name: 'Forum', params: { id: this.forum['.key'] } })
     }
   }
 }
