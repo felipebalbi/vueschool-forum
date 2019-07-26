2e0ac5b246f0ad69d6608f5f31f568f8080573cc
Felipe Balbi <balbi@kernel.org>
2019-07-26 22:24:57 +0300

    Install Firebase Realtime Database
    'yarn add firebase'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import AppDate from '@/components/AppDate'
@@ -8,6 +9,19 @@ import store from '@/store'
 
 Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC-uE_36ZgehlyD-Oh7bnz1AfAEa8Hk-Dw',
+  authDomain: 'vue-school-forum-5468b.firebaseapp.com',
+  databaseURL: 'https://vue-school-forum-5468b.firebaseio.com',
+  projectId: 'vue-school-forum-5468b',
+  storageBucket: '',
+  messagingSenderId: '883747564910',
+  appId: '1:883747564910:web:fab993eeb65de7bf'
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
