75a56cef6bdcb4413fbc00f62a19baaa5e769e1c
Felipe Balbi <balbi@kernel.org>
2019-07-26 22:33:24 +0300

    Set up the Firebase Realtime DatabaseRemoved all .key attributes from data.json and imported it to firebaserealtime database

--- src/main.js
+++ src/main.js
@@ -11,15 +11,7 @@ Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyC-uE_36ZgehlyD-Oh7bnz1AfAEa8Hk-Dw',
-  authDomain: 'vue-school-forum-5468b.firebaseapp.com',
-  databaseURL: 'https://vue-school-forum-5468b.firebaseio.com',
-  projectId: 'vue-school-forum-5468b',
-  storageBucket: '',
-  messagingSenderId: '883747564910',
-  appId: '1:883747564910:web:fab993eeb65de7bf'
-}
+const firebaseConfig = {}
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
