11d880ca0f780e4a3931a77a24d6dfca9eda3d8e
Felipe Balbi <balbi@kernel.org>
2019-07-27 21:47:47 +0300

    Fetch Data from Firebase With Vuex Actions

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -24,7 +24,6 @@
 </template>
 
 <script>
-import * as firebase from 'firebase'
 import PostList from '@/components/PostList'
 import PostEditor from '@/components/PostEditor'
 import { countObjectProperties } from '@/utils'
@@ -68,58 +67,15 @@ export default {
   },
 
   created () {
-    // fetch thread
-    firebase
-      .database()
-      .ref('threads')
-      .child(this.id)
-      .once('value', snapshot => {
-        const thread = snapshot.val()
-        this.$store.commit('setThread', {
-          threadId: snapshot.key,
-          thread: { ...thread, '.key': snapshot.key }
-        })
+    this.$store.dispatch('fetchThread', { id: this.id }).then(thread => {
+      this.$store.dispatch('fetchUser', { id: this.userId })
 
-        // fetch user
-        firebase
-          .database()
-          .ref('users')
-          .child(thread.userId)
-          .once('value', snapshot => {
-            const user = snapshot.val()
-            this.$store.commit('setUser', {
-              userId: snapshot.key,
-              user: { ...user, '.key': snapshot.key }
-            })
-          })
-
-        Object.keys(thread.posts).forEach(postId => {
-          // fetch Post
-          firebase
-            .database()
-            .ref('posts')
-            .child(postId)
-            .once('value', snapshot => {
-              const post = snapshot.val()
-              this.$store.commit('setPost', {
-                postId: snapshot.key,
-                post: { ...post, '.key': snapshot.key }
-              })
-              // fetch user
-              firebase
-                .database()
-                .ref('users')
-                .child(post.userId)
-                .once('value', snapshot => {
-                  const user = snapshot.val()
-                  this.$store.commit('setUser', {
-                    userId: snapshot.key,
-                    user: { ...user, '.key': snapshot.key }
-                  })
-                })
-            })
+      Object.keys(thread.posts).forEach(postId => {
+        this.$store.dispatch('fetchPost', { id: postId }).then(post => {
+          this.$store.dispatch('fetchUser', { id: post.userId })
         })
       })
+    })
   }
 }
 </script>
