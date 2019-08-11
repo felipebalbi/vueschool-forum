ff49fb7a21b004259afa14fc3314e5254afb6a35
Felipe Balbi <balbif@gmail.com>
2019-08-11 10:40:23 +0300

    Extract Custom Vuelidate Validators to a Utility

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -109,15 +109,13 @@
 </template>
 
 <script>
+import { required, email, minLength, url } from 'vuelidate/lib/validators'
 import {
-  required,
-  email,
-  minLength,
-  url,
-  helpers as vuelidateHelpers
-} from 'vuelidate/lib/validators'
-import firebase from 'firebase'
-import { Promise } from 'q'
+  uniqueUsername,
+  uniqueEmail,
+  responseOk,
+  supportedImageFile
+} from '@/utils/validators'
 
 export default {
   data () {
@@ -140,43 +138,13 @@ export default {
 
       username: {
         required,
-        unique (value) {
-          if (!vuelidateHelpers.req(value)) {
-            return true
-          }
-
-          return new Promise((resolve, reject) => {
-            firebase
-              .database()
-              .ref('users')
-              .orderByChild('usernameLower')
-              .equalTo(value.toLowerCase())
-              .once('value', snapshot => {
-                resolve(!snapshot.exists())
-              })
-          })
-        }
+        unique: uniqueUsername
       },
 
       email: {
         required,
         email,
-        unique (value) {
-          if (!vuelidateHelpers.req(value)) {
-            return true
-          }
-
-          return new Promise((resolve, reject) => {
-            firebase
-              .database()
-              .ref('users')
-              .orderByChild('email')
-              .equalTo(value.toLowerCase())
-              .once('value', snapshot => {
-                resolve(!snapshot.exists())
-              })
-          })
-        }
+        unique: uniqueEmail
       },
 
       password: {
@@ -186,26 +154,8 @@ export default {
 
       avatar: {
         url,
-        supportedImageFile (value) {
-          if (!vuelidateHelpers.req(value)) {
-            return true
-          }
-
-          const supported = ['jpg', 'jpeg', 'gif', 'png', 'svg']
-          const suffix = value.split('.').pop()
-          return supported.includes(suffix)
-        },
-        responseOk (value) {
-          if (!vuelidateHelpers.req(value)) {
-            return true
-          }
-
-          return new Promise((resolve, reject) => {
-            fetch(value)
-              .then(response => resolve(response.ok))
-              .catch(() => resolve(false))
-          })
-        }
+        supportedImageFile,
+        responseOk
       }
     }
   },
